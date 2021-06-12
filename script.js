aaee74d3d40b15f6dcc83b69a93d0540d281fdd4
Weinsaft Matan <weinsaftmatan@gmail.com>
2021-06-13 01:31:09 +0300

    Responsive Hamburger menu html,css,js, gallery html file, link

---
+++ script.js
@@ -0,0 +1,17 @@
+//Hamburger
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navlinks = document.querySelectorAll('.nav-link li');
+
+
+    burger.addEventListener('click',()=>{
+        
+        nav.classList.toggle('main-nav');
+    });
+
+   
+}
+
+navSlide();
